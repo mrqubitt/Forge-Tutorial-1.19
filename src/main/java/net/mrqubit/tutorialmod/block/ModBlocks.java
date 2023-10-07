46648d690eeb0a25d6d4eb7beb3111492444f7d0
mrqubitt <kaani.ylmz.45@gmail.com>
2023-10-07 12:22:07 +0300

    Added an eight ball that outputs and gets into cooldown when right clicked. Our first advanced item that uses triggers.

--- src/main/java/net/mrqubit/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/mrqubit/tutorialmod/block/ModBlocks.java
@@ -23,11 +23,21 @@ public class ModBlocks {
     //We create the deferred register here.
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, TutorialMod.MOD_ID);
 
+
+
+
+
+
     //Here we define an object to register as a block.
     public static final RegistryObject<Block> ZIRCON_BLOCK = registerBlock("zircon_block", () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.TUTORIAL_TAB);
     public static final RegistryObject<Block> ZIRCON_ORE = registerBlock("zircon_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(6f).requiresCorrectToolForDrops() , UniformInt.of(3,7)), ModCreativeModeTab.TUTORIAL_TAB);
     public static final RegistryObject<Block> DEEPSLATE_ZIRCON_ORE = registerBlock("deepslate_zircon_ore", () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE).strength(6f).requiresCorrectToolForDrops() , UniformInt.of(3,7)), ModCreativeModeTab.TUTORIAL_TAB);
     //--------------------------------------------------
+
+
+
+
+
 
     //Here we define an object to register as everything that extends to block.
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
